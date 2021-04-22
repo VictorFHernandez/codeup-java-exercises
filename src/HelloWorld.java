a0fb31073f4f94065453b711108fb6607c5668a5
VictorFHernandez <victorfhernandez3@gmail.com>
2021-04-22 16:45:38 -0500

    completed the first part of the syntax exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello, World!");
         System.out.print("\n");
+        int myFavoriteNumber = 78;
+        System.out.print(myFavoriteNumber);
+
     }
 }
