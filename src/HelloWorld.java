77614d858643061808519e67d106b81f120100a9
VictorFHernandez <victorfhernandez3@gmail.com>
2021-04-23 11:42:11 -0500

    assigned the value of 3.14 to myNumber variable

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
         System.out.println(myFavoriteNumber);
         double myString = 3.14159;
         System.out.println(myString);
-        long myNumber;
+        long myNumber = 3.14;
         System.out.println(myNumber);
     }
 }
