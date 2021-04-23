8c6d1fb28a1b458e766b8f1ada4f3ab9a1127a2d
VictorFHernandez <victorfhernandez3@gmail.com>
2021-04-23 15:14:07 -0500

    finished exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -18,21 +18,38 @@ public class HelloWorld {
 //        Object o = theNumberThree;
 //        int three = (int) o;
 //        int three = (int) "three";
-        int x = 4;
-        x = x + 5;
-        System.out.println(x);
+//
+//        int x = 4;
+//        x = x + 5;
+//        System.out.println(x);
 
-        int a = 3;
-        int b = 4;
-        b = b * a;
-        System.out.println(b);
+//        int x = 4;
+//        x += 5;
 
-        int m = 10;
-        int n = 2;
-        m = m / n;
-        n = n - m;
-        System.out.println(m);
-        System.out.println(n);
 
+//        int a = 3;
+//        int b = 4;
+//        b = b * a;
+//        System.out.println(b);
+
+//        int a = 3;
+//        int b = 4;
+//        b *= a;
+
+//        int m = 10;
+//        int n = 2;
+//        m = m / n;
+//        n = n - m;
+//        System.out.println(m);
+//        System.out.println(n);
+
+//        int m = 10;
+//        int n = 2;
+//        m /= n;
+//        n -= m;
+
+//        int z = Integer.MAX_VALUE;
+//        z+=1;
+//        System.out.println("z = " + z);
     }
 }
