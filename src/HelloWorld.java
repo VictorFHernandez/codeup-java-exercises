df7fe7dbb20d0a73d18348678465046689fb528e
VictorFHernandez <victorfhernandez3@gmail.com>
2021-04-23 12:02:32 -0500

    tried both codes and the first one gave me a error for not having the correct declarations and the second one was a incompatible error

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,19 +1,23 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
-        System.out.print("\n");
-        int myFavoriteNumber = 78;
-        System.out.println(myFavoriteNumber);
-        double myString = 3.14159;
-        System.out.println(myString);
-        double myNumber = 3.14;
-        System.out.println(myNumber);
-        int x = 5;
-        System.out.println(x++);
-        System.out.println(x);
-        int y = 5;
-        System.out.println(++y);
-        System.out.println(y);
-        int class;
+//        System.out.print("Hello, World!");
+//        System.out.print("\n");
+//        int myFavoriteNumber = 78;
+//        System.out.println(myFavoriteNumber);
+//        double myString = 3.14159;
+//        System.out.println(myString);
+//        double myNumber = 3.14;
+//        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int y = 5;
+//        System.out.println(++y);
+//        System.out.println(y);
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+        int three = (int) "three";
+
     }
 }
