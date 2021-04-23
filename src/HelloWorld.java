75bb9983203565012362cde144321b273b21adb6
VictorFHernandez <victorfhernandez3@gmail.com>
2021-04-23 11:55:30 -0500

    on the first example it logged the original value assigned afterwrds it incremented the value by one and on the secound example it logged pre incremented the value before logging it

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,7 +6,13 @@ public class HelloWorld {
         System.out.println(myFavoriteNumber);
         double myString = 3.14159;
         System.out.println(myString);
-        float myNumber = 3.14;
+        double myNumber = 3.14;
         System.out.println(myNumber);
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+        int y = 5;
+        System.out.println(++y);
+        System.out.println(y);
     }
 }
