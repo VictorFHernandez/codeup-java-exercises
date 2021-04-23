883896cbcb4908d166a84877fd57215b3e367df3
VictorFHernandez <victorfhernandez3@gmail.com>
2021-04-23 11:43:24 -0500

    change the value of my myNuber variable to 123L and it logged just the 123 with no errors

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
         System.out.println(myFavoriteNumber);
         double myString = 3.14159;
         System.out.println(myString);
-        long myNumber = 3.14;
+        long myNumber = 123L;
         System.out.println(myNumber);
     }
 }
