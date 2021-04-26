5a19dd634f8e4fd03b7d42b6c26193a7e27bd89c
VictorFHernandez <victorfhernandez3@gmail.com>
2021-04-26 15:22:02 -0500

    part B-2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -10,14 +10,23 @@ public class ControlFlowExercises {
         } */
 
 
-        /* Exercise 1 Part B:
+        /* Exercise 1 Part B-1:
 
         int maxNumber = 100;
         int count = 0;
         do{
             System.out.println(count += 2);
-        }while(count < maxNumber);  */
+        }while(count < maxNumber);
+        */
 
+        /* Exercise 1 Part B-2:
+
+        int maxNum = 105;
+        int minNum = -10;
+        do{
+            System.out.println(maxNum -= 5);
+        }while(maxNum > minNum);
+        */
 
     }
 }
