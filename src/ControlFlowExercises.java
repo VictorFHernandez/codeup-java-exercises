1f8a64c50456d68a4756c7426157de8251c5c4ef
VictorFHernandez <victorfhernandez3@gmail.com>
2021-04-26 16:19:35 -0500

    finished exercise 1 part C

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,32 +1,34 @@
 public class ControlFlowExercises {
     public static void main(String[] args){
 
-        /* Exercise 1 part A:
+//        Exercise 1 part A:
 
         int i = 5;
         while(i <= 15){
-            System.out.println("i is " + i);
+            System.out.println(i);
             i++;
-        } */
+        }
 
 
-        /* Exercise 1 Part B-1:
+//         Exercise 1 Part B-1:
 
         int maxNumber = 100;
         int count = 0;
         do{
             System.out.println(count += 2);
         }while(count < maxNumber);
-        */
 
-        /* Exercise 1 Part B-2:
+
+//        Exercise 1 Part B-2:
 
         int maxNum = 105;
         int minNum = -10;
         do{
             System.out.println(maxNum -= 5);
         }while(maxNum > minNum);
-        */
+
+
+//         exercise 1 part B-3:
 
         long startingNumber = 2;
         long leMillion = 1000000;
@@ -35,7 +37,22 @@ public class ControlFlowExercises {
             startingNumber *= startingNumber;
         }while (startingNumber < leMillion);
 
+//        exercise 1 part C:
 
+        for(int l = 5; l <= 15; l += 1){
+            System.out.println(l);
+        }
+
+        for(int p = 0; p <= 100; p += 2){
+            System.out.println(p);
+        }
+
+        for(int o = 100; o >= -10; o -= 5){
+            System.out.println(o);
+        }
+        for(long k = 2; k < leMillion; k *= k){
+            System.out.println(k);
+        }
 
     }
 }
