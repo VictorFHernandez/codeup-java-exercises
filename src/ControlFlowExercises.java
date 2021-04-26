05d044e1ed2677e5c964d8be3effcfb5f5e4788b
VictorFHernandez <victorfhernandez3@gmail.com>
2021-04-26 16:35:38 -0500

    finished exercise 2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,10 @@
 public class ControlFlowExercises {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
 //        Exercise 1 part A:
 
         int i = 5;
-        while(i <= 15){
+        while (i <= 15) {
             System.out.println(i);
             i++;
         }
@@ -14,45 +14,58 @@ public class ControlFlowExercises {
 
         int maxNumber = 100;
         int count = 0;
-        do{
+        do {
             System.out.println(count += 2);
-        }while(count < maxNumber);
+        } while (count < maxNumber);
 
 
 //        Exercise 1 Part B-2:
 
         int maxNum = 105;
         int minNum = -10;
-        do{
+        do {
             System.out.println(maxNum -= 5);
-        }while(maxNum > minNum);
+        } while (maxNum > minNum);
 
 
 //         exercise 1 part B-3:
 
         long startingNumber = 2;
         long leMillion = 1000000;
-        do{
+        do {
             System.out.println(startingNumber);
             startingNumber *= startingNumber;
-        }while (startingNumber < leMillion);
+        } while (startingNumber < leMillion);
 
 //        exercise 1 part C:
 
-        for(int l = 5; l <= 15; l += 1){
+        for (int l = 5; l <= 15; l += 1) {
             System.out.println(l);
         }
 
-        for(int p = 0; p <= 100; p += 2){
+        for (int p = 0; p <= 100; p += 2) {
             System.out.println(p);
         }
 
-        for(int o = 100; o >= -10; o -= 5){
+        for (int o = 100; o >= -10; o -= 5) {
             System.out.println(o);
         }
-        for(long k = 2; k < leMillion; k *= k){
+        for (long k = 2; k < leMillion; k *= k) {
             System.out.println(k);
         }
 
+//        exercise 2:
+
+        for (int m = 1; m <= 100; m++) {
+            if ((m % 3 == 0) && (m % 5 == 0)) {
+                System.out.println("fizzbuzz");
+            } else if (m % 3 == 0){
+                System.out.println("fizz");
+            } else if (m % 5 == 0){
+            System.out.println("buzz");
+            } else {
+                System.out.println(m);
+            }
+        }
     }
 }
