09629f4d4821aab8b7c850fe5b66089e2515ec71
VictorFHernandez <victorfhernandez3@gmail.com>
2021-04-26 15:13:37 -0500

    finished exercise 1 part B

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,23 @@
 public class ControlFlowExercises {
     public static void main(String[] args){
+
+        /* Exercise 1 part A:
+
         int i = 5;
         while(i <= 15){
             System.out.println("i is " + i);
             i++;
-        }
+        } */
+
+
+        /* Exercise 1 Part B:
+
+        int maxNumber = 100;
+        int count = 0;
+        do{
+            System.out.println(count += 2);
+        }while(count < maxNumber);  */
+
+
     }
 }
