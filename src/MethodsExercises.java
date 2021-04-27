f7c4832d56da9c25d147940c646e845ae8c2eb61
VictorFHernandez <victorfhernandez3@gmail.com>
2021-04-27 14:28:22 -0500

    finished exercise 1

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,9 +1,35 @@
 public class MethodsExercises {
+//    exercise 1:
+
     public static void main(String[] args){
-
-
-
-
-
+        System.out.println(Addition(2,2));
+        System.out.println(Subtraction(6,2));
+        System.out.println(Multiplication(2,2));
+        System.out.println(Division(8,2));
+        System.out.println(Modulus(100,8));
     }
+    public static int Addition(int numberOne, int numberTwo){
+        return numberOne + numberTwo;
+    }
+    public static int Subtraction(int numberOne, int numberTwo){
+        return numberOne - numberTwo;
+    }
+    public static int Multiplication(int numberOne, int numberTwo){
+        return numberOne * numberTwo;
+    }
+    public static int Division(int numberOne, int numberTwo){
+        return numberOne / numberTwo;
+    }
+    public static int Modulus(int numberOne, int numberTwo){
+        return numberOne % numberTwo;
+    }
+
+
+
+
+
+
+
+
+
 }
