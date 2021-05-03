b26a66885dfb3b1561f5b6e20dbc0b1e01b8e35e
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-03 12:15:48 -0500

    finished the getString part of the exercise

--- src/util/Input.java
+++ src/util/Input.java
@@ -8,20 +8,22 @@ public class Input {
 
     public static void main(String[] args){
     Input input = new Input();
+    System.out.println(input.getSting());
     System.out.println(input.yesNo());
 }
 
-//String getSting(){
-//    System.out.println("please enter a word");
-//    String input = scanner.nextLine();
-//    if(!input.equals("")){
-//        return input;
-//    }else{
-//        return input;
-//    }
-//}
+    String getSting(){
+    System.out.println("please enter a word");
+    String input = scanner.nextLine();
+    if(!input.equals("")){
+        return input + " is a string";
+    }else{
+        String notInput = "not a string";
+        return notInput;
+    }
+}
 
-    public boolean yesNo(){
+    boolean yesNo(){
     System.out.println("enter yes or no");
     String input = scanner.nextLine();
     return input.equalsIgnoreCase("yes") || input.equalsIgnoreCase("no");
