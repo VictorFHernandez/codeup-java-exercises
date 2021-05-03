3c4f9956fd56229ad89fbb7066ffe9e3a4f8ca52
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-03 17:00:44 -0500

    added adjetives array and nouns array

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,3 +1,5 @@
 public class ServerNameGenerator {
 
+    String[] adjetives = {"big", "cool", "scary", "brave", "happy", "powerful", "fierce", "great", "small", "colossal"};
+    String[] nouns = {"robot", "cat", "monkey", "state", "ocean", "bird", "army", "team", "lion", "banana"};
 }
