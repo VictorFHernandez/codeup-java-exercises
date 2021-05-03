4c98cdfe79f742187eb7c3e0c263863bb84baa2c
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-03 12:09:47 -0500

    finished yes or no part of the exercise

--- src/util/Input.java
+++ src/util/Input.java
@@ -3,21 +3,28 @@ package util;
 import java.util.Scanner;
 
 public class Input {
-//    private class scanner;
 
     Scanner scanner = new Scanner(System.in);
 
-    public String getString() {
-        String input = scanner.nextLine();
+    public static void main(String[] args){
+    Input input = new Input();
+    System.out.println(input.yesNo());
+}
 
-        System.out.println("Enter a string.");
-        input = scanner.nextLine();
-        if (!input.equals("")){
-            return input;
-        }
-        else {
-            System.out.println("Please enter a string");
-            return getString();
-        }
+//String getSting(){
+//    System.out.println("please enter a word");
+//    String input = scanner.nextLine();
+//    if(!input.equals("")){
+//        return input;
+//    }else{
+//        return input;
+//    }
+//}
+
+    public boolean yesNo(){
+    System.out.println("enter yes or no");
+    String input = scanner.nextLine();
+    return input.equalsIgnoreCase("yes") || input.equalsIgnoreCase("no");
     }
 }
+
