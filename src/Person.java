1d7f4f6f9c11178cedc13745e0a24339347215ba
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-03 09:54:50 -0500

    finished exercise 1

--- src/Person.java
+++ src/Person.java
@@ -1,21 +1,31 @@
 public class Person {
     private String name;
 
-//    public String getName(){
-////TODO: return the person's name
-//
-//    }
+    public Person(String name){
+        this.name = name;
+    }
+    public static void main(String[] args){
+        Person jim = new Person("jim");
+        System.out.println(jim.getName());
+        jim.setName("Dwight");
+        jim.sayHello();
+    }
+
+    public String getName(){
+//TODO: return the person's name
+        return name;
+    }
 
     public void setName(String name){
 //TODO: change the name property to the passed value
+        this.name = name;
 
     }
 
     public void sayHello(){
 //TODO: print a message to the console using the person's name
-
+        System.out.println(name + " Says Hello");
     }
-
 
 
 }
