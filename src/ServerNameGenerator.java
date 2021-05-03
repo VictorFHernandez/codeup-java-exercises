7cf576800781b4320000bd0b4cf6360aa4f66a51
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-03 17:21:41 -0500

    finished exercise 2

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,5 +1,15 @@
 public class ServerNameGenerator {
 
-    String[] adjetives = {"big", "cool", "scary", "brave", "happy", "powerful", "fierce", "great", "small", "colossal"};
-    String[] nouns = {"robot", "cat", "monkey", "state", "ocean", "bird", "army", "team", "lion", "banana"};
+    static void randomGenerator() {
+
+        String[] adjetives = {"big", "cool", "scary", "brave", "happy", "powerful", "fierce", "great", "small", "colossal"};
+        String[] nouns = {"robot", "cat", "monkey", "state", "ocean", "bird", "army", "team", "lion", "banana"};
+        String randomAdjetive = adjetives[(int) Math.floor(Math.random() * (adjetives.length - 1))];
+        String randomNouns = nouns[(int) Math.floor(Math.random() * (nouns.length - 1))];
+        System.out.println("Your Server name is:");
+        System.out.println(randomAdjetive + "-" + randomNouns);
+    }
+    public static void main(String[] args){
+        randomGenerator();
+    }
 }
