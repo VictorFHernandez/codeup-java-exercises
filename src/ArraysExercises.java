bb34bbd714b65cdc9018da2de23520cd33d1c0fe
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-03 16:11:28 -0500

    experimented with the first part of the exercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,6 +1,12 @@
+import java.util.Arrays;
+
 public class ArraysExercises {
     public static void main(String[] args){
 
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(numbers);
+//        the results come back like this {[I@5acf9800}
+        System.out.println(Arrays.toString(numbers));
 
     }
 }
