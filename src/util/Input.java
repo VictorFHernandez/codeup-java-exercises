7f2c97bad9aec738f1ae99376755e8793e5a42f9
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-03 10:49:43 -0500

    working on exercise 2

--- src/util/Input.java
+++ src/util/Input.java
@@ -6,4 +6,18 @@ public class Input {
 //    private class scanner;
 
     Scanner scanner = new Scanner(System.in);
+
+    public String getString() {
+        String input = scanner.nextLine();
+
+        System.out.println("Enter a string.");
+        input = scanner.nextLine();
+        if (!input.equals("")){
+            return input;
+        }
+        else {
+            System.out.println("Please enter a string");
+            return getString();
+        }
+    }
 }
