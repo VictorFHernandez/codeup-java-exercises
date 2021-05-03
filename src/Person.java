f47643b519f697a737302ba0b56a8eed2d22a3bf
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-03 10:04:58 -0500

    tried out the secound code in the curriculum and it came back true

--- src/Person.java
+++ src/Person.java
@@ -9,12 +9,19 @@ public class Person {
         System.out.println(jim.getName());
         jim.setName("Dwight");
         jim.sayHello();
+        /*
         Person person1 = new Person("John");
         Person person2 = new Person("John");
-        /* System.out.println(person1.getName().equals(person2.getName()));
+         System.out.println(person1.getName().equals(person2.getName()));
         System.out.println(person1 == person2);
         the first line came back true because it used the .equals
         and the second one came back false because it used the double equals sign(==) */
+       /*
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1 == person2);
+        the code above came back as true
+        */
     }
 
     public String getName(){
