a7a6ee7bb9de59ef1349bf13f72b6e9172d1949d
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-03 16:48:39 -0500

    added serverNameGenerator class

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,12 +1,17 @@
 import java.util.Arrays;
 
 public class ArraysExercises {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(numbers);
 //        the results come back like this {[I@5acf9800}
         System.out.println(Arrays.toString(numbers));
+        System.out.println();
+        String[] Person = {"Tony", "Steve", "Bruce"};
+        for (String Avenger : Person) {
+            System.out.println(Avenger);
+            //need to work on second part:
 
+        }
     }
 }
