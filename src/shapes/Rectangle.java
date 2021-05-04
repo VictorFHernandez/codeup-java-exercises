b72df0ad1fae385197ff7a72163efd34ba57619d
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-04 15:19:15 -0500

    created the two constructors for the rectangle class

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,4 +1,20 @@
 package shapes;
 
 public class Rectangle {
+    private int length(int input){
+        return input;
+    }
+
+    private int width(int input){
+        return input;
+    }
+
+    public int getArea(){
+        return length(5) * width(4);
+    }
+
+    public int getPerimeter(){
+        return (2 * length(5) + (2 * width(4)));
+    }
+
 }
