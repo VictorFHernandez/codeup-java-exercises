28e62b2d698a2161f8c502fbc15c2806a100e64c
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-04 11:26:33 -0500

    update

--- src/practice.java
+++ src/practice.java
@@ -1,9 +1,16 @@
 public class practice {
     public static void main(String[] args){
+        System.out.println("Hey! lets get it started");
+        System.out.println();
+        randomNumber();
+
+
+
 
     }
-
     static void randomNumber(){
-        double numbers = Math.random() * 100;
+        double numbers = Math.ceil(Math.random() * 100);
+        System.out.println("Your welcome code is " + numbers);
+        System.out.println("Don't forget ;)");
     }
 }
