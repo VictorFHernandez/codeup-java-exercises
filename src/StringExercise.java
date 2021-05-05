d8d1e33c82ac72e31d43e83ccd5a53b8648bd4dc
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-05 09:12:25 -0500

    added and updated the strings exercise with kenneths review

---
+++ src/StringExercise.java
@@ -0,0 +1,25 @@
+public class StringExercise {
+    public static void main(String[] args){
+
+        //First snippet: some lyrics!
+        String lyrics = "We don't need no education\nWe don't need no thought control!";
+
+        System.out.println(lyrics);
+        //Second snippet: Messing with escape chars!
+        String escape = "Check \"this\" out!, \"s inside of \"s!";
+
+        System.out.println(escape);
+
+        //Third snippet: Unescape the escape character :(
+        String windowsSentence = "In windows, the main drive is usually C:\\";
+        System.out.println(windowsSentence);
+
+        //Fourth snippet: Backslash show-off!
+        String backslash = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\";
+
+        System.out.println(backslash);
+
+
+
+    }
+}
