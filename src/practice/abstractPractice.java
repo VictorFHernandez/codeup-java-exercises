4da055dad67af9e8b17461919671bebcd652d96e
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-06 13:28:21 -0500

    update

--- src/practice/abstractPractice.java
+++ src/practice/abstractPractice.java
@@ -1,7 +1,21 @@
 package practice;
 
+abstract class dog {
+    String breed;
+
+    public void bark(){
+        System.out.println("Bark!");
+    }
+}
+
+class  dachshund extends dog{
+
+}
+
+
 public class abstractPractice {
     public static void main(String[] args){
-
+        dachshund lucky = new dachshund();
+        lucky.bark();
     }
 }
