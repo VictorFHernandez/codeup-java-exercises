1352e8a3986e154baff14e5134b7defd5d658922
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-06 13:36:47 -0500

    added the measurable interface

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,17 +1,17 @@
 package shapes;
 
 public class Rectangle {
-    protected int lenght;
-    protected int width;
-
-    public Rectangle(int lenght, int width){
-        this.lenght = lenght;
-        this.width = width;
-    }
-    public int getArea(){
-        return lenght * width;
-    }
-    public int getPerimeter(){
-        return (2 * lenght) + (2 * width);
-    }
+//    protected int lenght;
+//    protected int width;
+//
+//    public Rectangle(int lenght, int width){
+//        this.lenght = lenght;
+//        this.width = width;
+//    }
+//    public int getArea(){
+//        return lenght * width;
+//    }
+//    public int getPerimeter(){
+//        return (2 * lenght) + (2 * width);
+//    }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,16 +1,16 @@
 package shapes;
 
 public class Square extends Rectangle {
-    protected int side;
-    public Square(int side){
-        super(side, side);
-        this.side = side;
-    }
-    public int getArea(){
-        return (int) Math.pow(this.side, 2);
-    }
-    public int getPerimeter(){
-        return 4 * this.side;
-    }
+//    protected int side;
+//    public Square(int side){
+//        super(side, side);
+//        this.side = side;
+//    }
+//    public int getArea(){
+//        return (int) Math.pow(this.side, 2);
+//    }
+//    public int getPerimeter(){
+//        return 4 * this.side;
+//    }
 }
 
