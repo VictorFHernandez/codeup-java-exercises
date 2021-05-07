f6384460e24d4fa3baa06cb31605be26fd912f90
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-07 12:06:36 -0500

    working on exercise 1 of collectors and added the GradesApplication class with a main method

--- src/grades/Students.java
+++ src/grades/Students.java
@@ -3,17 +3,22 @@ package grades;
 import java.util.ArrayList;
 
 public class Students {
-    private ArrayList<Integer> grades = new ArrayList<>();
+    private ArrayList<Integer> Grades = new ArrayList<>();
+
+    private String name;
 
     public String getName(){
-
+        return this.name;
     }
 
     public void addGrade(int grade){
-
+        Grades.add(grade);
     }
 
-    public double getGradeAverage(){
+//    public double getGradeAverage(){
+//
+//    }
+    public static void main(String[] args){
 
     }
 }
