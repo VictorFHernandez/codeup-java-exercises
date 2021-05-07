161337f04410e7c469a17047a0ebd04d48400c3c
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-07 11:52:13 -0500

    added the collections students exercise

---
+++ src/grades/Students.java
@@ -0,0 +1,19 @@
+package grades;
+
+import java.util.ArrayList;
+
+public class Students {
+    private ArrayList<Integer> grades = new ArrayList<>();
+
+    public String getName(){
+
+    }
+
+    public void addGrade(int grade){
+
+    }
+
+    public double getGradeAverage(){
+
+    }
+}
