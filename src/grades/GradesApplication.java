8214bc9ef6b308528f8c89532bd292ef08160578
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-07 12:17:11 -0500

    added a HashMap named students

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,8 +1,15 @@
 package grades;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class GradesApplication {
     public static void main(String[] args){
-
+        Map<String, String> students = new HashMap<>();
+        students.put("K1mP0ss1bl3", "Kim");
+        students.put("J0hnC3Na", "Jonh");
+        students.put("B4dbUnny", "Ben");
+        students.put("Sp1derM4N", "Peter");
 
     }
 }
