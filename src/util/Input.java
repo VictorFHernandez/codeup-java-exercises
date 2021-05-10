419f1628ef3a9aec9d6b4ce118a3a4c7fdf9c244
VictorFHernandez <victorfhernandez3@gmail.com>
2021-05-10 12:29:40 -0500

    completed the exeptions exercise

--- src/util/Input.java
+++ src/util/Input.java
@@ -53,30 +53,29 @@ public class Input {
         }
 
     public double getDouble(double min, double max){
-        System.out.println("Enter a decimal number between " + min + " and " + max + ".");
+        System.out.println("please enter a decimal number between " + min + " and " + max);
         double input = scanner.nextDouble();
-        scanner.nextLine();
-        if (input >= min && input <= max){
-            return input;
-        }
-        else {
-            System.out.println("The input is not within the parameters.");
+        if (input >= min && input <= max) {
+            System.out.println("Thank you! " + input + " is a valid decimal number");
+        } else {
+            System.out.println("Invalid decimal number");
             return getDouble(min, max);
         }
-    };
+        return input;
+    }
 
     public double getDouble(){
-        System.out.println("Enter a decimal number.");
-        if (scanner.hasNextDouble()){
-            double input  = scanner.nextDouble();
-            scanner.nextLine();
-            return input;
-        }
-        else {
-            String input  = scanner.next();
-            scanner.nextLine();
-            System.out.println("The input " + input +  " is not a decimal.");
-            return getDouble();
+        double input;
+        while (true){
+            try{
+                System.out.println("Please enter a decimal number");
+                input = Double.valueOf(scanner.next());
+                System.out.println(input + " is a valid decimal number. Thank you for your cooperation :)");
+                return input;
+            }
+            catch(Exception e){
+                System.out.println("Sorry, that is NOT a decimal number");
+            }
         }
     }
 }
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -5,12 +5,11 @@ import java.util.Scanner;
 public class InputTest {
     public static void main(String[] args){
         Input input = new Input();
-//        System.out.println(input.getSting());
-//        System.out.println(input.yesNo());
-//        System.out.println(input.getInteger(1, 10));
+        System.out.println(input.getSting());
+        System.out.println(input.yesNo());
+        System.out.println(input.getInteger(1, 10));
         System.out.println(input.getInt());
-
-
-
+        System.out.println(input.getDouble(1, 10));
+        System.out.println(input.getDouble());
     }
 }
